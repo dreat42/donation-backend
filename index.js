044ddacaaffb8bf7ad42e0c46a45840f51a8d764
dreat42 <72696242+dreat42@users.noreply.github.com>
2021-06-16 03:21:04 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+require('./src/models/User');
+require('./src/models/feedback');
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./src/routes/authRoutes');
+
+const requireAuth = require('./src/middlewares/requireAuth');
+const requestRouter = require('./src/routes/requestRoute');
+const feedback = require('./src/routes/feedback');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(requestRouter);
+app.use(feedback);
+const port = process.env.PORT || 5000;
+
+const mongoUri = process.env.ATLAS_URI;
+if (!mongoUri) {
+  throw new Error(
+    `MongoURI was not supplied.  Make sure you watch the video on setting up Mongo DB!`,
+  );
+}
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', err => {
+  console.error('Error connecting to mongo', err);
+});
+
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port: ${port}`);
+});
---
+++ src/models/RequestForm.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const requestSchema = new Schema(
+  {
+    username: {type: String, required: true},
+    title: {type: String, required: true},
+    email: {type: String, required: true},
+    description: {type: String, required: true},
+    fundrequired: {type: Number, required: true},
+    fundraised: {type: Number, required: true},
+    upi: {type: String, required: true},
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Request = mongoose.model('Request', requestSchema);
+
+module.exports = Request;
